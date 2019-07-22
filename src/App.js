ccab452be7e75e91d7115ecb8c9f9406a650d1c9
NiravPatel111 <48572469+NiravPatel111@users.noreply.github.com>
2019-07-22 14:06:40 -0400

    CSS styling done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './Components/Button';
+import Display from './Components/Display';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='App'>
+          <Display currentValue={this.state.currentVal} />
+          <Button
+            initialize={this.initialize}
+            numbers={this.handleNumber}
+            operators={this.handleOperators}
+            decimal={this.handleDecimal}
+            evaluate={this.handleEvaluate}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Button.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button
+          id='clear'
+          value='AC'
+          onClick={this.props.initialize}
+          className='clearbtn'
+        >
+          AC
+        </button>
+        <button id='modulus' value='%' onClick={this.props.operators}>
+          %
+        </button>
+        <button id='divide' value='/' onClick={this.props.operators}>
+          /
+        </button>
+        <button id='seven' value='7' onClick={this.props.numbers}>
+          7
+        </button>
+        <button id='eight' value='8' onClick={this.props.numbers}>
+          8
+        </button>
+        <button id='nine' value='9' onClick={this.props.numbers}>
+          9
+        </button>
+        <button id='multiply' value='x' onClick={this.props.operators}>
+          x
+        </button>
+        <button id='four' value='4' onClick={this.props.numbers}>
+          4
+        </button>
+        <button id='five' value='5' onClick={this.props.numbers}>
+          5
+        </button>
+        <button id='six' value='6' onClick={this.props.numbers}>
+          6
+        </button>
+        <button id='subtract' value='-' onClick={this.props.operators}>
+          -
+        </button>
+        <button id='one' value='1' onClick={this.props.operators}>
+          1
+        </button>
+        <button id='two' value='2' onClick={this.props.operators}>
+          2
+        </button>
+        <button id='three' value='3' onClick={this.props.operators}>
+          3
+        </button>
+        <button id='add' value='+' onClick={this.props.operators}>
+          +
+        </button>
+        <button id='zero' value='+' onClick={this.props.operators}>
+          0
+        </button>
+        <button id='decimal' value='.' onClick={this.props.decimal}>
+          .
+        </button>
+        <button id='equals' value='+' onClick={this.props.evaluate}>
+          =
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Button;
