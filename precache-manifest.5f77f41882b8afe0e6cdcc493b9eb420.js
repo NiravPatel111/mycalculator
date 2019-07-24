7b86e41ee297be5429a22fe66fd0489045164aa9
NiravPatel111 <48572469+NiravPatel111@users.noreply.github.com>
2019-07-24 14:47:43 -0400

    Updates

---
+++ precache-manifest.5f77f41882b8afe0e6cdcc493b9eb420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c51378b0718f5640e1f38bf7dc9cd0",
+    "url": "/mycalculator/index.html"
+  },
+  {
+    "revision": "ab077f59953d6b1054c7",
+    "url": "/mycalculator/static/css/main.c7973846.chunk.css"
+  },
+  {
+    "revision": "fe63233c9c975bfaa6ba",
+    "url": "/mycalculator/static/js/2.141ae541.chunk.js"
+  },
+  {
+    "revision": "ab077f59953d6b1054c7",
+    "url": "/mycalculator/static/js/main.2a63f042.chunk.js"
+  },
+  {
+    "revision": "8b33e20d773e5812c7b4",
+    "url": "/mycalculator/static/js/runtime~main.c0a9aa15.js"
+  }
+]);
